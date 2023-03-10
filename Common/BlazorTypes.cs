e0697fd10fb2e64c54f753ade0f630bbc330e400
Alejandro Cernuda <alejandro@acernuda.com>
2023-03-10 13:52:02 +0100

    Adding LinkCollectionComponent.  <BlazorCommon.Component.LinkCollection LinkCollectionType ="LinkCollectionType.Horizontal" Items="@Items" FollowThis="acernuda.com" />

--- Common/BlazorTypes.cs
+++ Common/BlazorTypes.cs
@@ -29,4 +29,11 @@ namespace BlazorCommon
         Grid,
         Main
     }
+
+    public enum LinkCollectionType
+    {
+        Vertical,
+        Horizontal,
+        ButtonHorizontal
+    }
 }
---
+++ Component/Model/SimpleIndexItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorCommon
+{
+    public class LinkCollectionItem
+    {
+        public string Name { get; set; }
+        public string path { get; set; }
+        public bool isTargetBlank { get; set; }
+    }
+}
